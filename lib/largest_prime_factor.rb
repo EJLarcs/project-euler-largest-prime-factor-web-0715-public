1f526473e7e38cffc9728317d60f929d896384fc
EJLarcs <ejlarcombe@gmail.com>
2015-10-12 22:38:46 -0400

    Crazy Big Nambas

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,29 @@
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+  increasing_divider = 2
+  i = (num/increasing_divider).to_i
+  highest_prime_factor = nil
+  while i < num
+    if num % i == 0 && is_prime?(i)
+      highest_prime_factor = i
+      break
+    else
+      increasing_divider += 1
+      i = (num/increasing_divider).to_i
+    end
+  end
+  highest_prime_factor
+end
+
+def is_prime?(i)
+   n = 2
+     loop do
+       if n == i
+         return true
+       elsif i % n == 0
+         return false
+       else
+         n += 1
+       end
+     end
+ end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,36 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    increasing_divider = 2
+    i = (@num/increasing_divider).to_i
+    highest_prime_factor = nil
+    while i < @num
+      if @num % i == 0 && is_prime?(i)
+        highest_prime_factor = i
+        break
+      else
+        increasing_divider += 1
+        i = (@num/increasing_divider).to_i
+      end
+    end
+    highest_prime_factor
+  end
+
+  def is_prime?(i)
+     n = 2
+       loop do
+         if n == i
+           return true
+         elsif i % n == 0
+           return false
+         else
+           n += 1
+         end
+       end
+   end
+end
